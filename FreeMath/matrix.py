6d15e3dff28bb8880a36ab42682876a2e9bc6040
Clayton McCray <cpmdmccray@gmail.com>
2017-10-18 23:18:52 -0500

    Structure library as a package

--- FreeMath/matrix.py
+++ FreeMath/matrix.py
@@ -170,23 +170,3 @@ class Matrix(MatrixOperations):
         return self.matrix
 
 
-# TODO
-# Vector class needs cross and dot product methods
-class Vector(Matrix):
-
-    @staticmethod
-    def dot(a, b, w):
-        u = [[]]
-        v = []
-        for row in a.matrix:
-            for col in row:
-                u[0].append(col)
-        for row in b.matrix:
-            for col in row:
-                v.append([col])
-        q = Vector.build(a.get_name(), u)
-        p = Vector.build(b.get_name(), v)
-        return Vector.matrix_product(q, p, w, vector=True)
-
-
-
